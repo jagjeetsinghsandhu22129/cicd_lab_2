570c5517953f518696422b83afda3396fbb2b2c3
Jagjeet Singh <jagjeetsinghsandhu22129@gmail.com>
2024-10-22 21:25:32 -0400

    files modified

--- test/server.test.js
+++ test/server.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should respond with Hello from Node.js backend!', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello from Node.js backend!');
+    expect(response.text).toBe('Midterm Exam Successfully Done!');
   });
 });
